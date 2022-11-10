26edcf9a56fbb8fe1b8ab581c061fdb6e0f12339
salva <salvationordeath@gmail.com>
2022-11-10 16:27:27 +0700

    ДЗ  - задачи с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ДЗ 1 - задача 1:");
-        int i = 1;
-        for (i = 1; i <= 10; i++) {
-            System.out.println(i);
+        System.out.println("ДЗ 2 - задача 1:");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
         }
 
-        System.out.println("ДЗ 1 - задача 2:");
-        int j = 10;
-        for (j = 10; j >= 1; j--) {
-            System.out.println(j);
+        System.out.println("ДЗ 2 - задача 2:");
+        for (int j = 7; j <= 98; j = j + 7) {
+            System.out.print(j + " ");
         }
+        System.out.println(" ");
 
-        System.out.println("ДЗ 1 - задача 3:");
-        int k = 0;
-        for (k = 0; k <= 17; k++) {
-            if (k % 2 == 0) {
-                System.out.println(k);
-            }
-        }
-
-        System.out.println("ДЗ 1 - задача 4:");
-        for (j = 10; j >= -10; j--) {
-            System.out.println(j);
+        System.out.println("ДЗ 2 - задача 3:");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.print(i + " ");
         }
     }
 }
