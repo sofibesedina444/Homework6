b4f5b69f469416de9236c913545c7f961fab64b2
salva <salvationordeath@gmail.com>
2022-11-10 16:35:31 +0700

    ДЗ 2 - задачи с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ДЗ 2 - задача 1:");
         for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println(i + " год является високосным");
+            System.out.println(i + " год является високосным.");
         }
 
         System.out.println("ДЗ 2 - задача 2:");
