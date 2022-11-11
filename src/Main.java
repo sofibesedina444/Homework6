edd8075cb23dc3061ff43a9d8bf5de14aaefab8a
salva <salvationordeath@gmail.com>
2022-11-11 18:09:49 +0700

    ДЗ 1 часть 2 - задачи с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ДЗ 3 - задача 1:");
-        int investment = 29000;
-        int savings = 0;
-        for (int i = 1; i <= 12; i++) {
-            savings = savings + investment;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings + " рублей");
-        }
 
-        System.out.println("ДЗ 3 - задача 2:");
-        int investment1 = 29000;
-        int savings1 = 0;
-        for (int i = 1; i <= 12; i++) {
-            savings1 = savings1 + savings1 / 100;
-            savings1 = savings1 + investment1;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + savings1 + " рублей");
+        System.out.println("ДЗ 1 часть 2 - задача 1:");
+        double investment = 15000;
+        double savings = 0;
+        int month = 0;
+        while (savings <= 2459000) {
+            savings = savings + investment;
+            month++;
+        }
+        System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей");
+
+        System.out.println("ДЗ 1 часть 2 - задача 2:");
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int j = 10; j >= 1; j--) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        System.out.println("ДЗ 1 часть 2 - задача 3:");
+        int population = 12_000_000;
+        int fertility = 17;
+        int mortality = 8; // рождаемость и смертность на 1000 человек за 1 год
+        int growth = (population / 1000) * (fertility - mortality); // прирост численности за 1 год
+        for (int years = 1; years <= 10; years++) {
+            population = population + growth;
+            System.out.println("Год " + years + ", численность населения составляет " + population);
         }
     }
 }
